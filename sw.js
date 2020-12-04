90a23d626c094bfb6e3fcd6f4debdb2df9db5286
JorgeMig <40581398+JorgeMigCM@users.noreply.github.com>
2020-12-04 11:47:02 -0400

    piuma 2.2

--- sw.js
+++ sw.js
@@ -3,9 +3,9 @@
 importScripts('js/sw_utils.js');
 
 
-const STATIC_CACHE = 'static-v1';
-const DYNAMIC_CACHE = 'dynamic-v1';
-const INMUTABLE_CACHE = 'inmutable-v1';
+const STATIC_CACHE = 'static-v2';
+const DYNAMIC_CACHE = 'dynamic-v2';
+const INMUTABLE_CACHE = 'inmutable-v2';
 
 
 
@@ -21,6 +21,7 @@ const APP_SHELL = [
 ];
 
 const APP_SHELL_INMUTABLE = [
+    'http://fonts.googleapis.com/css?family=Lato:300,400,700',
     'js/jquery-1.11.0.min.js',
     'js/scroll.js',
     'js/jquery.countdown.js',
